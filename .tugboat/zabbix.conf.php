e42fb9cda8472fc8e8ae27736681ee27e8afbbec
James Sansbury <james.sansbury@tugboatqa.com>
2025-11-04 12:40:57 -0500

    initial tugboat config

---
+++ .tugboat/zabbix.conf.php
@@ -0,0 +1,71 @@
+<?php
+// Zabbix GUI configuration file.
+
+$DB['TYPE']			= 'MYSQL';
+$DB['SERVER']			= 'mariadb';
+$DB['PORT']			= '0';
+$DB['DATABASE']			= 'zabbix';
+$DB['USER']			= 'zabbix';
+$DB['PASSWORD']			= 'zabbix';
+
+// Schema name. Used for PostgreSQL.
+$DB['SCHEMA']			= '';
+
+// Used for TLS connection.
+$DB['ENCRYPTION']		= true;
+$DB['KEY_FILE']			= '';
+$DB['CERT_FILE']		= '';
+$DB['CA_FILE']			= '';
+$DB['VERIFY_HOST']		= false;
+$DB['CIPHER_LIST']		= '';
+
+// Vault configuration. Used if database credentials are stored in Vault secrets manager.
+$DB['VAULT']			= '';
+$DB['VAULT_URL']		= '';
+$DB['VAULT_PREFIX']		= '';
+$DB['VAULT_DB_PATH']		= '';
+$DB['VAULT_TOKEN']		= '';
+$DB['VAULT_CERT_FILE']		= '';
+$DB['VAULT_KEY_FILE']		= '';
+// Uncomment to bypass local caching of credentials.
+// $DB['VAULT_CACHE']		= true;
+
+// Uncomment and set to desired values to override Zabbix hostname/IP and port.
+// $ZBX_SERVER			= '';
+// $ZBX_SERVER_PORT		= '';
+
+$ZBX_SERVER_NAME		= 'Zabbix';
+
+$IMAGE_FORMAT_DEFAULT		= IMAGE_FORMAT_PNG;
+
+// Uncomment this block only if you are using Elasticsearch.
+// Elasticsearch url (can be string if same url is used for all types).
+//$HISTORY['url'] = [
+//	'uint' => 'http://localhost:9200',
+//	'text' => 'http://localhost:9200'
+//];
+// Value types stored in Elasticsearch.
+//$HISTORY['types'] = ['uint', 'text'];
+
+// Used for SAML authentication.
+
+// Uncomment to set extra settings.
+//$SSO['SETTINGS']		= [];
+
+// Set to 'file' to store the private key and certificates on the file system.
+$SSO['CERT_STORAGE']		= 'database';
+
+// Uncomment to override the default paths for the private key and certificates, if stored on the file system.
+//$SSO['SP_KEY']		= 'conf/certs/sp.key';
+//$SSO['SP_CERT']		= 'conf/certs/sp.crt';
+//$SSO['IDP_CERT']		= 'conf/certs/idp.crt';
+
+// If set to false, support for HTTP authentication will be disabled.
+// $ALLOW_HTTP_AUTH = true;
+
+$ZBX_SERVER_TLS['ACTIVE'] = '0';
+$ZBX_SERVER_TLS['CA_FILE'] = '';
+$ZBX_SERVER_TLS['KEY_FILE'] = '';
+$ZBX_SERVER_TLS['CERT_FILE'] = '';
+$ZBX_SERVER_TLS['CERTIFICATE_ISSUER']  = '';
+$ZBX_SERVER_TLS['CERTIFICATE_SUBJECT'] = '';
